a0b2e34fbaff27900bb25f658acaf9520c48c722
xxb87089890 <xuxiaobin91@163.com>
2018-06-10 18:49:41 +0800

    add city router

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hearder">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+.hearder {
+  position: relative;
+  height: $headerHeight;
+  line-height: $headerHeight;
+  background: $bgColor;
+  text-align: center;
+  color: #fff;
+  font-size: .32rem;
+  .back-icon {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: .64rem;
+    float: left;
+    text-align: center;
+    font-size: .4rem;
+    color: #fff;
+  }
+}
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="hearder-city">
-      {{this.city}}
-      <span class="iconfont arrow-icon">&#xe64a;</span>
-    </div>
+    <router-link to="/city">
+      <div class="hearder-city">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -24,40 +26,41 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .hearder {
-    display: flex;
-    line-height: .86rem;
-    background: $bgColor;
-    color: #fff;
-    .hearder-return {
-      width: .64rem;
-      float: left;
-      .back-icon {
-        text-align: center;
-        font-size: .4rem;
-      }
-    }
-    .hearder-input {
-      height: .64rem;
-      -webkit-flex: 1;   /* Chrome */
-      -ms-flex: 1;       /* IE 10 */
-      flex: 1;
-      background: #fff;
-      color: #ccc;
-      border-radius: 4px;
-      margin-top: .12rem;
-      margin-bottom: .2rem;
-      padding-left: .2rem;
-      line-height: .64rem;
-    }
-    .hearder-city {
-      float: right;
-      width: 1.24rem;
+.hearder {
+  display: flex;
+  line-height: $headerHeight;
+  background: $bgColor;
+  color: #fff;
+  .hearder-return {
+    width: .64rem;
+    float: left;
+    .back-icon {
       text-align: center;
-      .arrow-icon {
-        font-size: .24rem;
-        margin-left: -.04rem;
-      }
+      font-size: .4rem;
     }
   }
+  .hearder-input {
+    height: .64rem;
+    -webkit-flex: 1;   /* Chrome */
+    -ms-flex: 1;       /* IE 10 */
+    flex: 1;
+    background: #fff;
+    color: #ccc;
+    border-radius: 4px;
+    margin-top: .12rem;
+    margin-bottom: .2rem;
+    padding-left: .2rem;
+    line-height: .64rem;
+  }
+  .hearder-city {
+    float: right;
+    width: 1.24rem;
+    text-align: center;
+    color: #fff;
+    .arrow-icon {
+      font-size: .24rem;
+      margin-left: -.05rem;
+    }
+  }
+}
 </style>
